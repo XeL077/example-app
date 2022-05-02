838e584d97fc9c674feeb591fdce6b8ba38fda0a
Alexander Panin <a.panin@petrovich.ru>
2022-05-02 17:37:46 +0300

    Refactoring structure server and docker

--- node-app/index.js
+++ node-app/index.js
@@ -1,24 +1,30 @@
 import Koa from 'koa';
+import Router from 'koa-router';
 import logger from 'koa-morgan';
 import env from 'dotenv';
 import { createClient } from 'redis';
 
 env.config();
+
 const server = new Koa();
+const router = new Router();
 
 const redisClient = createClient({
     url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
     password: process.env.REDIS_PASSWORD,
 });
 await redisClient.connect();
-redisClient.set('test', 'dsd');
+redisClient.set('test', 'dsddfd');
+
+router.get('/test', async (ctx) => {
+    ctx.body = await redisClient.get('test');
+})
+
 
 server
     .use(logger('tiny'))
-    .use(async (ctx) => {
-        ctx.body =  await redisClient.get('test');
-    })
-    .listen(process.env.PORT, () => {
+    .use(router.routes())
+    .listen(process.env.PORT || 8080, () => {
         console.log(`server start listening on port ${process.env.PORT}`);
         redisClient.ping().then(status => console.log(status));
 });
